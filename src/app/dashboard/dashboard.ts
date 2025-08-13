da31313a6f87e56776d679acb241ed29cdc1199e
phillipnzau <nzauonline@gmail.com>
2025-08-13 12:27:34 +0300

    feat: topbar init

--- src/app/dashboard/dashboard.ts
+++ src/app/dashboard/dashboard.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from '../shared/components/navbar/navbar';
+import { TopBar } from '../shared/components/top-bar/top-bar';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [RouterOutlet, Navbar],
+  imports: [RouterOutlet, Navbar, TopBar],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css',
 })
