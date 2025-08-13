b3ab568b4f029237e29e651a68aabc09eeacff39
phillipnzau <nzauonline@gmail.com>
2025-08-13 13:14:41 +0300

    feat: sidenav working

--- src/app/shared/components/navbar/navbar.ts
+++ src/app/shared/components/navbar/navbar.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './navbar.html',
-  styleUrl: './navbar.css'
+  styleUrl: './navbar.css',
 })
 export class Navbar {
-
+  router = inject(Router);
+  isRouteActive(routePath: string): boolean {
+    return this.router.url.includes(routePath);
+  }
 }
