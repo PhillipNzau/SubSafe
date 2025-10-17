c51ff0f2db86b8944927898340c2a59bd15e191a
phillipnzau <nzauonline@gmail.com>
2025-10-17 14:40:57 +0300

    feat: mobile cards scrollable

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,12 @@
-const url = 'http://localhost:8080/';
+// const url = 'http://localhost:8080/';
+const url = 'https://go-vault.onrender.com/';
 
 const authBaseUrl = url + 'auth/';
 const credentialsBaseUrl = url + 'credentials';
 const subscriptionsBaseUrl = url + 'subscriptions';
+const hubsBaseUrl = url + 'hubs';
+const exportBaseUrl = url + 'export';
+const importBaseUrl = url + 'import';
 
 export const environment = {
   production: true,
@@ -10,16 +14,34 @@ export const environment = {
   ///////////////** USER URLS **///////////////////
   loginUser: authBaseUrl + 'login',
   registerUser: authBaseUrl + 'register',
+  refreshToken: authBaseUrl + 'refresh',
+  verifyOtp: authBaseUrl + 'verify-otp',
   ///////////////** CREDENTIALS URLS **///////////////////
   listCredentials: credentialsBaseUrl,
   listSingleCredential: credentialsBaseUrl + '/',
-  deleteCredential: credentialsBaseUrl,
+  deleteCredential: credentialsBaseUrl + '/',
   createCredential: credentialsBaseUrl,
   updateCredential: credentialsBaseUrl + '/',
   ///////////////** SUBSCRIPTION URLS **///////////////////
   listSubscriptions: subscriptionsBaseUrl,
   listSingleSubscription: subscriptionsBaseUrl + '/',
-  deleteSubscription: subscriptionsBaseUrl,
+  deleteSubscription: subscriptionsBaseUrl + '/',
   createSubscription: subscriptionsBaseUrl,
   updateSubscription: subscriptionsBaseUrl + '/',
+
+  ///////////////** EXPORT IMPORT URLS **///////////////////
+  exportCredentials: exportBaseUrl + '/credentials/excel',
+  exportSubscriptions: exportBaseUrl + '/subscriptions/excel',
+  exportResources: exportBaseUrl + '/resources/excel',
+
+  importCredentials: importBaseUrl + '/credentials/excel',
+  importSubscriptions: importBaseUrl + '/subscriptions/excel',
+  importResources: importBaseUrl + '/resources/excel',
+
+  ///////////////** HUBS URLS **///////////////////
+  listHubs: hubsBaseUrl,
+  listSingleHub: hubsBaseUrl + '/',
+  deleteHub: hubsBaseUrl + '/delete/',
+  createHub: hubsBaseUrl,
+  updateHub: hubsBaseUrl + '/update/',
 };
